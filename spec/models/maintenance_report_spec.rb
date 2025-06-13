dd7543dc27e8a8acf3998169a43aec588408b5ed
Daniel Zamora <daniel.zamoraf@gmail.com>
2025-06-12 18:43:16 -0700

    Add maintenance report model filters and validations

--- spec/models/maintenance_report_spec.rb
+++ spec/models/maintenance_report_spec.rb
@@ -21,83 +21,85 @@ RSpec.describe MaintenanceReport, type: :model do
   end
 
   describe "validations" do
-    xit { should validate_presence_of(:description) }
-    xit { should validate_presence_of(:priority) }
-    xit { should validate_presence_of(:status) }
-    xit { should validate_presence_of(:reported_at) }
-    xit { should validate_presence_of(:vehicle) }
-    xit { should validate_presence_of(:user) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:priority) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:reported_at) }
   end
 
   context "boundary cases for reported_at" do
-    xit { should validate_datetime_of(:reported_at).is_before(Date.current) }
-
-    xit "is invalid if reported_at > current date" do
+    it "is invalid if reported_at > current date" do
       subject.reported_at = Date.current + 1.day
       expect(subject).to be_invalid
-      expect(subject.errors[:reported_at]).to include("must be before #{Date.current}")
+      expect(subject.errors[:reported_at]).to include("can't be in the future")
     end
   end
 
   describe "scopes" do
-    let!(:m1) { create(:maintenance_report, status: :pendiente) }
-    let!(:m2) { create(:maintenance_report, status: :procesado) }
-    let!(:m3) { create(:maintenance_report, status: :rechazado) }
+    let!(:user) { create(:user) }
+    let!(:vehicle) { create(:vehicle, user: user) }
+    let!(:m1) { create(:maintenance_report, status: :pendiente, user: user, vehicle: vehicle,
+                                            priority: :alta, reported_at: Date.today - 1.day) }
+    let!(:m2) { create(:maintenance_report, status: :procesado, user: user, vehicle: vehicle,
+                                            priority: :media, reported_at: Date.today - 2.day) }
+    let!(:m3) { create(:maintenance_report, status: :rechazado, user: user, vehicle: vehicle,
+                                            priority: :baja, reported_at: Date.today - 3.day) }
 
     context ".by_status" do
-      xit "returns only the maintenance_reports with the given status" do
+      it "returns only the maintenance_reports with the given status" do
         expect(MaintenanceReport.by_status("pendiente")).to match_array([ m1 ])
       end
 
-      xit "returns all if status is nil or empty string" do
+      it "returns all if status is nil or empty string" do
         expect(MaintenanceReport.by_status(nil)).to include(m1, m2, m3)
         expect(MaintenanceReport.by_status("")).to include(m1, m2, m3)
       end
     end
 
     context ".by_user" do
-      xit "returns only the maintenance_reports of the given user" do
+      it "returns only the maintenance_reports of the given user" do
         u = create(:user)
-        m_user = create(:maintenance_report, user: u)
+        v = create(:vehicle, user: u)
+        m_user = create(:maintenance_report, user: u, vehicle: v)
         expect(MaintenanceReport.by_user(u.id)).to eq([ m_user ])
       end
 
-      xit "returns all if user_id is nil or empty string" do
+      it "returns all if user_id is nil or empty string" do
         expect(MaintenanceReport.by_user(nil)).to include(m1, m2, m3)
         expect(MaintenanceReport.by_user("")).to include(m1, m2, m3)
       end
     end
 
     context ".by_vehicle" do
-      xit "returns only the maintenance_reports for the given vehicle" do
+      it "returns only the maintenance_reports for the given vehicle" do
         v = create(:vehicle)
-        m_vehicle = create(:maintenance_report, vehicle: v)
+        m_vehicle = create(:maintenance_report, vehicle: v, user: user)
         expect(MaintenanceReport.by_vehicle(v.id)).to eq([ m_vehicle ])
       end
 
-      xit "returns all if vehicle_id is nil or empty string" do
+      it "returns all if vehicle_id is nil or empty string" do
         expect(MaintenanceReport.by_vehicle(nil)).to include(m1, m2, m3)
         expect(MaintenanceReport.by_vehicle("")).to include(m1, m2, m3)
       end
     end
 
     context ".by_priority" do
-      xit "returns only the maintenance_reports with the given priority" do
-        expect(MaintenanceReport.by_priority("alta")).to match_array([ m1, m2, m3 ]) # Assuming all have 'alta' priority
+      it "returns only the maintenance_reports with the given priority" do
+        expect(MaintenanceReport.by_priority("alta")).to match_array([ m1 ])
       end
 
-      xit "returns all if priority is nil or empty string" do
+      it "returns all if priority is nil or empty string" do
         expect(MaintenanceReport.by_priority(nil)).to include(m1, m2, m3)
         expect(MaintenanceReport.by_priority("")).to include(m1, m2, m3)
       end
     end
 
     context ".by_reported_at range date" do
-      xit "returns only the maintenance_reports within the given date range" do
+      it "returns only the maintenance_reports within the given date range" do
         expect(MaintenanceReport.by_reported_at(Date.current - 1.day..Date.current)).to match_array([ m1 ])
       end
 
-      xit "returns all if date range is nil or empty" do
+      it "returns all if date range is nil or empty" do
         expect(MaintenanceReport.by_reported_at(nil)).to include(m1, m2, m3)
         expect(MaintenanceReport.by_reported_at("")).to include(m1, m2, m3)
       end
@@ -110,66 +112,67 @@ RSpec.describe MaintenanceReport, type: :model do
     let!(:v1) { create(:vehicle, user: u1) }
     let!(:v2) { create(:vehicle, user: u2) }
     let!(:m1) { create(:maintenance_report, user: u1, vehicle: v1, status: :pendiente,
-                                            reported_at: Date.today - 1.day) }
-    let!(:m2) { create(:maintenance_report, user: u2, vehicle: v2, status: :procesado) }
+                                            reported_at: Date.today - 1.day, priority: :baja) }
+    let!(:m2) { create(:maintenance_report, user: u2, vehicle: v2, status: :procesado,
+                                            priority: :baja) }
     let!(:m3) { create(:maintenance_report, user: u1, vehicle: v2, status: :rechazado,
                                             priority: :alta) }
 
-    xit "filters by status" do
+    it "filters by status" do
       filters = { status: "pendiente" }
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m1 ])
     end
 
-    xit "filters by user" do
+    it "filters by user" do
       filters = { user_id: u1.id }
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m1, m3 ])
     end
 
-    xit "filters by vehicle" do
+    it "filters by vehicle" do
       filters = { vehicle_id: v1.id }
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m1 ])
     end
 
-    xit "filters by priority" do
+    it "filters by priority" do
       filters = { priority: "alta" }
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m3 ])
     end
 
-    xit "filters by reported_at range" do
+    it "filters by reported_at range" do
       filters = { reported_at: Date.current - 1.day..Date.current }
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m1 ])
     end
 
-    xit "returns all if no filters are applied" do
+    it "returns all if no filters are applied" do
       filters = {}
       result = MaintenanceReport.filter(filters)
       expect(result).to match_array([ m1, m2, m3 ])
     end
 
-    xit "returns an empty array if no records match the filters" do
+    it "returns an empty array if no records match the filters" do
       filters = { status: "non_existent_status" }
       result = MaintenanceReport.filter(filters)
       expect(result).to be_empty
     end
 
-    xit "returns an empty array if filters are nil" do
+    it "returns all if filters are nil" do
       filters = nil
       result = MaintenanceReport.filter(filters)
-      expect(result).to be_empty
+      expect(result).to match_array([ m1, m2, m3 ])
     end
 
-    xit "returns an empty array if filters are empty" do
+    it "returns all if filters are empty" do
       filters = {}
       result = MaintenanceReport.filter(filters)
-      expect(result).to be_empty
+      expect(result).to match_array([ m1, m2, m3 ])
     end
 
-    xit "returns an empty array if no records match the filters" do
+    it "returns an empty array if no records match the filters" do
       filters = { status: "non_existent_status" }
       result = MaintenanceReport.filter(filters)
       expect(result).to be_empty
