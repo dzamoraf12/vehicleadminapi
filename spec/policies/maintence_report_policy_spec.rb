9f6ae0ae56bf46249a7f1064b40d8daf319c9a0c
Daniel Zamora <daniel.zamoraf@gmail.com>
2025-06-13 00:15:24 -0700

    Add test maintenance report policy

--- spec/policies/maintence_report_policy_spec.rb
+++ spec/policies/maintence_report_policy_spec.rb
@@ -8,41 +8,42 @@ RSpec.describe MaintenanceReportPolicy, type: :policy do
   context "when user is admin" do
     let(:current_user) { build_stubbed(:user, role: "admin") }
 
-    xit { is_expected.to permit_actions([ :index, :show, :update ]) }
-    xit { is_expected.to forbid_actions([ :create ]) } # For demostration purposes is forbidden
+    it { is_expected.to permit_actions([ :index, :show, :update ]) }
+    it { is_expected.to forbid_actions([ :create ]) } # For demostration purposes is forbidden
   end
 
   context "when user is technician" do
     let(:current_user) { build_stubbed(:user, role: "tecnico") }
 
-    xit { is_expected.to forbid_actions([ :index, :show, :create, :update ]) }
+    it { is_expected.to permit_actions([ :index, :show, :create, :update ]) }
   end
 
   context "when user is driver" do
     let(:current_user) { build_stubbed(:user, role: "chofer") }
 
-    xit { is_expected.to forbid_actions([ :create ]) }
+    it { is_expected.to permit_actions([ :create ]) }
+    it { is_expected.to forbid_actions([ :index, :show, :update ]) }
   end
 
   # test scope when user is admin and cant create maintenance reports
   context "scope" do
     let(:current_user) { build_stubbed(:user, role: "admin") }
 
-    xit "admin scope returns all maintenance reports" do
+    it "admin scope returns all maintenance reports" do
       scope = MaintenanceReportPolicy::Scope.new(current_user, MaintenanceReport.all).resolve
       expect(scope).to match_array(MaintenanceReport.all)
     end
 
     let(:current_user) { build_stubbed(:user, role: "tecnico") }
 
-    xit "technician scope returns all maintenance reports" do
+    it "technician scope returns all maintenance reports" do
       scope = MaintenanceReportPolicy::Scope.new(current_user, MaintenanceReport.all).resolve
       expect(scope).to match_array(MaintenanceReport.all)
     end
 
     let(:current_user) { build_stubbed(:user, role: "chofer") }
 
-    xit "driver scope returns none" do
+    it "driver scope returns none" do
       scope = MaintenanceReportPolicy::Scope.new(current_user, MaintenanceReport.all).resolve
       expect(scope).to match_array([])
     end
